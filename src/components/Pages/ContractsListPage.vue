987b7e2fa823b182a444c168ca7962a9333930b5
kl1mmm <klimovilya07@mail.ru>
2022-05-23 09:01:50 +0900

    Contracts table operable

--- src/components/Pages/ContractsListPage.vue
+++ src/components/Pages/ContractsListPage.vue
@@ -4,28 +4,21 @@
       <thead>
       <tr>
         <th scope="col">№</th>
-        <th scope="col"></th>
-        <th scope="col">Last</th>
-        <th scope="col">Handle</th>
+        <th scope="col">Дата подписания</th>
+        <th scope="col">Юр.лицо</th>
+        <th scope="col">ИНН</th>
+        <th scope="col">ОГРН</th>
+        <th scope="col">Дата окончания</th>
       </tr>
       </thead>
-      <tbody>
+      <tbody class="contract" v-for="contract in contracts" :key="contract.id">
       <tr>
-        <th scope="row">1</th>
-        <td>Mark</td>
-        <td>Otto</td>
-        <td>@mdo</td>
-      </tr>
-      <tr>
-        <th scope="row">2</th>
-        <td>Jacob</td>
-        <td>Thornton</td>
-        <td>@fat</td>
-      </tr>
-      <tr>
-        <th scope="row">3</th>
-        <td colspan="2">Larry the Bird</td>
-        <td>@twitter</td>
+        <th scope="row">{{ contract.id }}</th>
+        <td>{{ contract.DateSign }}</td>
+        <td>{{ contract.entit_cl }}</td>
+        <td>{{ contract.INN_cl }}</td>
+        <td>{{ contract.OGRN_cl }}</td>
+        <td>{{ contract.DateExpir }}</td>
       </tr>
       </tbody>
     </MyTable>
@@ -35,7 +28,14 @@
 <script>
 import MyTable from "@/components/UI/MyTable";
 export default {
-  components: {MyTable}
+  components: {MyTable},
+  data() {
+    return {
+      contracts: [
+        {id: '1', DateSign: '01.01.2022', entit_cl: 'Нет', INN_cl:'', OGRN_cl: '777666555', DateExpir:'01.01.2023'}
+      ]
+    }
+  }
 }
 </script>
 
